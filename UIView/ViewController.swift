4eb30685135cae2f33d12dd9641991b28eb7de7c
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-21 00:38:27 +0530

    The UIView class defines a rectangular area on the screen and the interfaces for managing the content in that area.

--- UIView/ViewController.swift
+++ UIView/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var simpleView=UIView(frame: CGRect(x: 100, y: 200, width: 100, height: 100))
+        simpleView.backgroundColor=UIColor.gray
+    
+        
+        
+        var roundedView=UIView(frame: CGRect(x: 100, y: 400, width: 100, height: 100))
+        roundedView.backgroundColor=UIColor.gray
+        roundedView.layer.cornerRadius=25
+        roundedView.layer.borderWidth=1
+        
+        self.view.addSubview(roundedView)
+    
+        self.view.addSubview(simpleView)
     }
 
     override func didReceiveMemoryWarning() {
